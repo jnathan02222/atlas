3e079ecf87c7fed08c7a5831ca9c1c5397c2c8aa
jnathan0222 <123518866+jnathan0222@users.noreply.github.com>
2025-01-14 21:44:09 -0500

    Authorization

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,44 @@
 //Switch to Go eventually
 
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+var client_id = '0626b416c6164a5599c9c2c4af16d0b7';
+var client_secret = '569fe22a11754975b18b10eb1bd52aef';
+
+function searchForPlaylist(){
+
+}
+function markPlaylist(){
+
+}
+
+const authOptions = {
+  method: 'post',
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  data: new URLSearchParams({
+    grant_type: 'client_credentials',
+  }).toString(),
+}
+
+axios(authOptions).then(response => {
+  if (response.status === 200) {
+    console.log(response.data)
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
 })
+app.listen(3000, ()=>{
+  console.log("Listening on 3000")
+})
+
