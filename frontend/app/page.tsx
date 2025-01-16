13e433a7efba4fd1ee48004a60761d9514c8cf86
Nathan <test@gmail.com>
2025-01-16 00:02:15 -0500

    ATLAS

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,12 +6,13 @@ export default function Home() {
         <div className="flex justify-center items-center animate-slow-spin aspect-square w-2/5">
           <img src="/Emblem_of_the_United_Nations.svg" className="w-full"></img>
         </div>
-        <div className="rounded-full bg-white w-[10%] aspect-square absolute"></div>
+        <div className="rounded-full bg-gray-800 w-[10%] aspect-square absolute"></div>
         <div className="rounded-full bg-black w-[2.5%] aspect-square absolute"></div>
       </div>
-      <div>
-        <h1 className="text-8xl">Atlas</h1>
-        <button className="border-2 p-2 rounded-md">Sign In</button>
+      <div className="flex flex-col items-center absolute">
+        <img src="Full_Logo_White_RGB.svg" className="h-6"></img>
+        <h1 className="text-8xl text-white">Atlas</h1>
+        <button className=" rounded-md text-white">Sign In</button>
       </div>
       
     </div>
