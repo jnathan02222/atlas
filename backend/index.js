15dd1d89d12fdeaadfcc62b2fda2eef11266a61c
Nathan <test@gmail.com>
2025-01-18 15:19:37 -0500

    Rotated API key

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,37 @@
 
 const express = require('express')
 const axios = require('axios')
+const querystring = require('querystring')
+require('dotenv').config();
+
 const app = express()
-const port = 3000
 
-var client_id = '0626b416c6164a5599c9c2c4af16d0b7';
-var client_secret = '569fe22a11754975b18b10eb1bd52aef';
+var client_id = '0626b416c6164a5599c9c2c4af16d0b7'
+var client_secret = process.env.SPOTIFY_CLIENT_SECRET 
+var redirect_uri = "http://localhost:8888/api/callback" //Also change
 
-function searchForPlaylist(keyword){
-  
+
+function searchForPlaylist(token, search){
+  axios({
+    method: 'get',
+    url : 'https://api.spotify.com/v1/search/',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+    },
+    params : {
+      q: search,
+      type: 'playlist'
+    }
+  }).then(
+    response => {
+      if (response.status === 200) {
+        console.log(response.data)
+      }
+    }
+  )
 }
 function markPlaylist(){
-
+  
 }
 
 const authOptions = {
@@ -20,7 +40,6 @@ const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
-    'Content-Type': 'application/x-www-form-urlencoded',
   },
   data: new URLSearchParams({
     grant_type: 'client_credentials',
@@ -29,16 +48,40 @@ const authOptions = {
 
 axios(authOptions).then(response => {
   if (response.status === 200) {
-    console.log(response.data)
+    searchForPlaylist(response.data.access_token, "sad")
   }
 })
 
 
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
-app.listen(3000, ()=>{
-  console.log("Listening on 3000")
-})
 
+
+
+
+
+//API backend
+function generateRandomString(n) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < n; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+app.get('/api/login', (req, res) => {
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+})
+app.listen(8888, ()=>{
+  console.log("Listening on 8888")
+})
